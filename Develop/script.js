aa72bd6d927835d14ba5e6a750dcd42b64e7f9ae
Asantowasso <79666127+Asantowasso@users.noreply.github.com>
2022-08-27 16:04:41 -0400

    added criteria eliminated syntax errors

--- Develop/script.js
+++ Develop/script.js
@@ -1,14 +1,24 @@
+
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
+  var chars= "0123456789abcdefghifklmonpqrstuvwxyz!@#$%^&*()ABCDEFHGHIJKLMNOPQRSTUVWXYZ"
+  var passwordLength = 12
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
-
+  
 }
+
+
+  writePassword.value = password;
+
+
+
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+
