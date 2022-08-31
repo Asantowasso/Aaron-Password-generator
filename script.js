90e971a8f090550d26075e65bc638bc38b8b8cf3
Asantowasso <79666127+Asantowasso@users.noreply.github.com>
2022-08-30 21:38:13 -0400

    added for loop and rand function

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
 // Assignment Code
 
 
-var choiceArr = ["a", "b", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "_", "+", "=","?"]
+var choiceArr = ["a", "b", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "_", "+", "=","?"];
 
 
 
 var generateBtn = document.querySelector("#generate");
+
 
 
 
@@ -24,6 +25,7 @@ function generatePassword() {
 
   // WHEN asked for character types to include in the password
   // THEN I confirm whether or not to include lowercase, uppercase, numeric, and/or special characters
+
 var lowercase = confirm ("lowercase?");
 var uppercase = confirm ("uppercase?");
 var numeric = confirm ("numeric");
